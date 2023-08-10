bfedc863abd5899c0bdb8624ed1153bf3fe448d1
bunju20 <ghkd2271@naver.com>
2023-08-11 01:19:25 +0900

    feat: 비로직 슬라이더 추가 완료.
    슬라이더 값이 웹으로 들어가게 만드는 부분만 남았음.

--- lib/screens/detail_settings.dart
+++ lib/screens/detail_settings.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 import 'preview_screen_settings.dart';
 
-class DetailSettingsScreen extends StatelessWidget {
+class DetailSettingsScreen extends StatefulWidget {
+  @override
+  _DetailSettingsScreenState createState() => _DetailSettingsScreenState();
+}
+
+class _DetailSettingsScreenState extends State<DetailSettingsScreen> {
+  File? _selectedImage;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      setState(() {
+        _selectedImage = File(pickedImage.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,14 +29,29 @@ class DetailSettingsScreen extends StatelessWidget {
         title: Text('Detail Settings Screen'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => PreviewScreenSettingsScreen()),
-            );
-          },
-          child: Text('Go to Preview Screen Settings'),//프리뷰 내 사용자 설정
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_selectedImage != null)
+              Image.file(
+                _selectedImage!,
+                height: 200,
+              ),
+            ElevatedButton(
+              onPressed: _pickImage,
+              child: Text('Select an Image from Gallery'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PreviewScreenSettingsScreen(imageFile: _selectedImage)),
+                );
+              },
+              child: Text('Go to Preview Screen Settings'),
+            ),
+          ],
         ),
       ),
     );
--- lib/screens/preview_screen_settings.dart
+++ lib/screens/preview_screen_settings.dart
@@ -1,14 +1,64 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-class PreviewScreenSettingsScreen extends StatelessWidget {
+class PreviewScreenSettingsScreen extends StatefulWidget {
+  final File? imageFile;
+  final String websiteUrl = 'https://f29f-61-72-189-152.ngrok-free.app/';
+
+  PreviewScreenSettingsScreen({required this.imageFile});
+
+  @override
+  _PreviewScreenSettingsScreenState createState() => _PreviewScreenSettingsScreenState();
+}
+
+class _PreviewScreenSettingsScreenState extends State<PreviewScreenSettingsScreen> {
+  double _sliderValue = 50; // 초기 슬라이더의 값은 50으로 설정
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Preview Screen Settings Screen'),
+        title: Text('Preview Screen Settings'),
       ),
-      body: Center(
-        child: Text('Preview Screen Settings Screen'),
+      body: Column(
+        children: [
+          Expanded(
+            child: InAppWebView(
+              initialUrlRequest: URLRequest(url: Uri.parse(widget.websiteUrl)),
+              onWebViewCreated: (controller) {
+                controller.evaluateJavascript(source: '''
+                  // JavaScript 코드를 여기에 넣어서 콘텐츠를 수정하거나 추가할 수 있습니다.
+                  // 예를 들어 배경화면을 전체 화면으로 확대하거나 이미지를 가운데로 정렬하는 등의 작업을 수행할 수 있습니다.
+                ''');
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('0%'),
+                Expanded(
+                  child: Slider(
+                    value: _sliderValue,
+                    onChanged: (value) {
+                      setState(() {
+                        _sliderValue = value;
+                      });
+                    },
+                    min: 0,
+                    max: 100,
+                    divisions: 100,
+                    label: '${_sliderValue.round()}%',
+                  ),
+                ),
+                Text('100%'),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
