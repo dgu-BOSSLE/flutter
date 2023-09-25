6ea8a8366afd2749bb2d17fd90a3bf947d519a00
34-43 <121276581+34-43@users.noreply.github.com>
2023-09-26 00:52:46 +0900

    feat: colors.dart 추가, 비 내리는 홈 화면

---
+++ lib/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SeedColors {
+  static TextStyle font = GoogleFonts.getFont('Gowun Dodum');
+  static TextStyle font_w = GoogleFonts.getFont('Gowun Dodum', textStyle: TextStyle(color: Colors.white));
+  static const Color darkest = Color(0xFF35155D);
+  static const Color darker = Color(0xFF512B81);
+  static const Color lighter = Color(0xFF4477CE);
+  static const Color lightest = Color(0xFF8CABFF);
+}
