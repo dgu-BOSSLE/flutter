3fe666b6454047611182aa9408f24fdac86827f6
34-43 <121276581+34-43@users.noreply.github.com>
2023-09-27 13:33:37 +0900

    Revert "Merge branch 'main' of https://github.com/dgu-BOSSLE/flutter"
    This reverts commit eca6b948c3e3f9295aa428d6816657cbb605ac1d, reversing
    changes made to a07a51076fe126339dc98690f9fecb03fe909526.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screens/home.dart';
-import 'screens/send_api.dart';
-import 'screens/globals.dart';
 
-void main() async{
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await GlobalVariables.loadPreferences();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((value) {
     runApp(MyApp());
@@ -17,30 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'My Flutter App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  MainScreen(), //  MainScreen
-    );
-  }
-}
-
-class MainScreen extends StatefulWidget {
-  @override
-  _MainScreenState createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(child: HomeScreen()), // HomeScreen 위젯
-          if(GlobalVariables.shouldCallApi ?? true) //나중에 false해야함.
-            Expanded(child: ApiCallOnTime()), // ApiCallOnTime 위젯
-        ],
-      ),
+      home: HomeScreen(), // HomeScreen
     );
   }
 }
